4e0f8f182cbc64cbac5c3636df1f009b063d241e
NewDelion <el.emena.crea@gmail.com>
2016-11-15 18:04:49 +0900

    commit

---
+++ SparkViewer/converter/TotalUsersConv.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SparkViewer.converter
+{
+    public class TotalUsersConv : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return string.Format("ユーザ数：{0}", (int)value);
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string v = value as string;
+            if (v.Length < 6)
+                return 0;
+            return int.Parse(v.Substring(6));
+        }
+    }
+}
